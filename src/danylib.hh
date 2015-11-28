7d2beb775c29834b9678af43af3d5e4d1641fad7
marineorganism <marineorganism@gmail.com>
2015-11-28 15:51:35 +1100

    adding new file io functions

--- src/danylib.hh
+++ src/danylib.hh
@@ -22,11 +22,17 @@ char * danylib_fit(char * data);
 //puts an array of chars into an array of a set size
 char * danylib_fitToLength(char * data,int length);
 
-//writes a numerical value to an array of characters
-template<typename T> char * danylib_valueToChars(T in);
+//writes a numerical value to an array of bytes
+template<typename T> uint8_t * danylib_valueToChars(T in);
 
-//turns four characters into a 32 bit integer
-template<typename T> T * danylib_charsToValue(char * chars);
+//turns an array of bytes into an integer type
+//the array must be the same size as the type
+template<typename T> T * danylib_charsToValue(uint8_t * in);
 
+//reads non byte data from a file into an array of bytes
+template<typename T> void danylib_readFromBytes(uint8_t * output,int nItems,FILE * input);
+
+//reads non byte data from a file into an array of bytes
+template<typename T> void danylib_writeToBytes(uint8_t * input,int nItems,FILE * output);
 
 #endif
--- src/numerical.cc
+++ src/numerical.cc
@@ -1,13 +1,15 @@
 #include "danylib.hh"
 
+#include <stdint.h>
+
 
 #define BYTE 8
 #define BYTE_MASK 0xFF
 
 
-template<typename T> char * danylib_valueToChars(T in)
+template<typename T> uint8_t * danylib_valueToChars(T in)
 {
-  char * out = new char[sizeof(T)];
+  uint8_t * out = new uint8_t[sizeof(T)];
 
   for (int i = 0;i < sizeof(T);i++)
   {
@@ -18,7 +20,7 @@ template<typename T> char * danylib_valueToChars(T in)
 }
 
 
-template<typename T> T * danylib_charsToValue(char * in)
+template<typename T> T * danylib_charsToValue(uint8_t * in)
 {
   T out = 0;
 
